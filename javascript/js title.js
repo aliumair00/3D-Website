d925e611221f99be7c48ebb2286f491e133d2d8f
aliumair00 <aliumair55013@gmail.com>
2024-07-16 15:22:53 +0500

    first commit

---
+++ javascript/js title.js
@@ -0,0 +1,117 @@
+/*
+// const product={
+//     name:"Dollar",
+//     rating:4,
+//     price:70,
+    
+// };
+// product['price']= product['price']+30
+// console.log(product["price"]);
+*/
+/*
+let a=19;
+let b=2;
+let cond1=a>b;
+let cond2=a===b;
+console.log("cond1&&cond2",cond1&&cond2);
+
+let age=29;
+result= age?"adult":"not adult";
+console.log(result);
+*/
+/*question check the number is multiple of 2 or not using prompt
+let num = prompt("Enter a number:");
+if(num%2==0){
+    console.log("num is multiple of 2");
+}
+else{
+    console.log("num is not multiple of 2");
+}
+*/
+/*which give grade to student
+let score=prompt("Enter number");
+if(score>=90 && score<=100){
+    Grade="A";
+}
+else if (score >=80 && score<=89){
+    Grade="B";
+}else if(score >=70 && score <=79){
+    Grade="C";
+}else if(score>=60&& score<=69){
+    Grade="D";
+}
+console.log("your grade is",Grade);
+*/
+// let n=6;
+// let sum=0;
+// for(let i=1;i<=n;i++){
+//     sum=sum+i;
+// }
+// console.log(sum);
+//For of loop used in array or string:
+// let str="Hello World!";
+// for(let i of str){
+//     console.log("i=",i);
+// }  
+//For in loop used in object or array:
+//for(let key in obj var)
+// let student={
+//     name:"Rabia",
+//     age:29,
+//     City:"lahore",
+
+// };
+// for(let key in student){
+//     console.log("key=","value",student[key]);
+// }
+//practice Q
+//  for (let i=1;i<=100;i++)
+//   {
+//      if(i%2===0){
+//      console.log("Even number is:",i);
+
+//      }
+//  }
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//       console.log("Even number is: ", i);
+//     }
+//   }
+// let name=prompt("Enter your name without spaces");
+// let username="@"+name+name.length
+// console.log(username);
+//Q:For given array find ave marks of class;
+// let marks=[85,97,44,37,76,60]
+//  let sum=0;
+//  for(let i=0;i<marks.length;i++){
+//      sum=sum+marks[i];
+// }
+//  let avg=sum/marks.length;
+//      console.log(avg);
+//Q:find  percent of values
+// let items=[250,645,300,900,50]
+// for(let i=0;i<items.length;i++){
+//     let offer=items[i]/10;
+//     items[i]=items[i]-offer;
+//     console.log("value after offer is =",items[i]); 
+// }
+//Q:
+//let companies =["bloomberg","microsoft","uber","google","ibm","netflix"]
+//console.log(companies);
+//Remove first company 
+//companies.splice(0,1);
+//Remove uber and ola in its place 
+//companies.splice(2,1,"Ola");
+//Add amazon at the end
+//companies.push("amazon")
+//console.log(companies);
+// let number=[1,2,3,4,5]
+// for(let i= number.length-1;i>=0;i--){ 
+//     console.log(number[i]);  
+// }
+// let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+// console.log(arr[i]);
+// for(let i=arr.length-1;i>=0;i--){
+//     console.log(arr[i]);
+// }
+
